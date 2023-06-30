9212309d9f1a3d2965be5b27d81217a39f9d69b5
N1ghting4le <nikansoloveib@gmail.com>
2023-06-30 22:14:11 +0300

    deployment commit

---
+++ .env.js
@@ -0,0 +1,3 @@
+module.exports = {
+    BASE_URL: 'https://chat-app-server-rpiq.onrender.com'
+};
--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     images: {
-        domains: ['localhost', 'img.icons8.com', '192.168.0.100', '192.168.0.102']
+        domains: ['localhost', 'img.icons8.com', '192.168.0.100', '192.168.0.102', 'chat-app-server-rpiq.onrender.com']
     }
 }
