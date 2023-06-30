e83965a041e5dc12220ce4f14482f694c3d6e254
N1ghting4le <nikansoloveib@gmail.com>
2023-06-30 22:39:38 +0300

    metadata error fix

--- app/[...login]/page.js
+++ app/[...login]/page.js
@@ -2,15 +2,12 @@
 
 const BASE_URL = process.env.BASE_URL;
 
+import Head from "next/head";
 import Header from "@/components/Header";
 import ButtonSection from "@/components/ButtonSection";
 import SSEProvider from "@/components/SSEProvider";
 
 import styles from "../../styles/userPage.module.css";
-
-export const metadata = {
-    description: 'Страница пользователя'
-};
 
 const getUserInfo = async (login) => {
     const res = await fetch(`${BASE_URL}/users/${login}`, {cache: "no-store"});
@@ -33,33 +30,38 @@ const userPage = async ({params}) => {
     const {name, surname, age, location, info, image} = userInfo;
 
     return (
-        <SSEProvider>
-            <Header/>
-            <main className={styles.main}>
-                <section className={styles.userInfo}>
-                    <img src={image ? `${BASE_URL}/${image}` : "https://img.icons8.com/material/400/7B92AD/camera--v1.png"} alt={`profile photo of ${name}`} className={styles.image} style={{'objectFit': 'cover'}}/>
-                    <div className={styles.textInfoWrapper}>
-                        <h1 className={styles.bold}>{name} {surname}</h1>
-                        {age ? 
-                        <div className={styles.infoString}>
-                            <span className={styles.bold}>Возраст:</span>
-                            <span>{age} {setAgeString(age)}</span>
-                        </div> : null}
-                        {location ? 
-                        <div className={styles.infoString}>
-                            <span className={styles.bold}>Место жительства:</span>
-                            <span>{location}</span>
-                        </div> : null}
-                        {info ? 
-                        <div>
-                            <span className={styles.bold}>Обо мне:</span>
-                            <p>{info}</p>
-                        </div> : null}
-                    </div>
-                </section>
-                <ButtonSection login={login} name={name} surname={surname} image={image}/>
-            </main>
-        </SSEProvider>
+        <>
+            <Head>
+                <meta aria-description="Страница профиля пользователя"/>
+            </Head>
+            <SSEProvider>
+                <Header/>
+                <main className={styles.main}>
+                    <section className={styles.userInfo}>
+                        <img src={image ? `${BASE_URL}/${image}` : "https://img.icons8.com/material/400/7B92AD/camera--v1.png"} alt={`profile photo of ${name}`} className={styles.image} style={{'objectFit': 'cover'}}/>
+                        <div className={styles.textInfoWrapper}>
+                            <h1 className={styles.bold}>{name} {surname}</h1>
+                            {age ? 
+                            <div className={styles.infoString}>
+                                <span className={styles.bold}>Возраст:</span>
+                                <span>{age} {setAgeString(age)}</span>
+                            </div> : null}
+                            {location ? 
+                            <div className={styles.infoString}>
+                                <span className={styles.bold}>Место жительства:</span>
+                                <span>{location}</span>
+                            </div> : null}
+                            {info ? 
+                            <div>
+                                <span className={styles.bold}>Обо мне:</span>
+                                <p>{info}</p>
+                            </div> : null}
+                        </div>
+                    </section>
+                    <ButtonSection login={login} name={name} surname={surname} image={image}/>
+                </main>
+            </SSEProvider>
+        </>
     );
 }
 
--- app/chats/page.js
+++ app/chats/page.js
@@ -3,20 +3,22 @@
 import Header from "@/components/Header";
 import ChatsList from "@/components/ChatsList";
 import SSEProvider from "@/components/SSEProvider";
-
-export const metadata = {
-    title: 'Ваши чаты',
-    description: 'Страница со списком чатов пользователя'
-};
+import Head from "next/head";
 
 const ChatsPage = () => {
     return (
-        <SSEProvider>
-            <Header/>
-            <main>
-                <ChatsList/>
-            </main>
-        </SSEProvider>
+        <>
+            <Head>
+                <meta title="Мои чаты"/>
+                <meta aria-description="Страница со списком чатов пользователя"/>
+            </Head>
+            <SSEProvider>
+                <Header/>
+                <main>
+                    <ChatsList/>
+                </main>
+            </SSEProvider>
+        </>
     );
 }
 
